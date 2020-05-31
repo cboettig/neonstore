1434ca065f68da3037cfd9224c2319942343618d
Carl <cboettig@gmail.com>
2020-05-31 22:58:16 +0000

    unzip. also be able to parse zip meta

--- R/api.R
+++ R/api.R
@@ -1,5 +1,9 @@
 
-
+#' Table of all NEON sites
+#' 
+#' Returns a table of all NEON sites by making a single API call
+#' to the `/sites` endpoint.
+#' @inheritParams neon_download
 #' @importFrom httr GET content
 #' @importFrom jsonlite fromJSON
 neon_sites <- function(api = "https://data.neonscience.org/api/v0", 
@@ -12,13 +16,64 @@ neon_sites <- function(api = "https://data.neonscience.org/api/v0",
  
 }
 
-neon_products <- function(api = "https://data.neonscience.org/api/v0", 
-                       .token = Sys.getenv("NEON_TOKEN")){
+#' Table of all NEON Data Products
+#'
+#' Return a table of all NEON Data Products, including product descriptions
+#' and the productCode needed for [neon_download].  
+#' @param fields a list of fields (columns) to include.  Default includes
+#' most common columns, set to NULL to display all columns
+#' (including list-columns).
+#' @inheritParams neon_download
+#' @seealso [neon_download]
+#' @export
+#' @examples 
+#' \donttest{
+#' 
+#' products <- neon_products()
+#' 
+#' # Or search for a keyword
+#' i <- grepl("bird", products$keyword)
+#' products[i, c("productCode", "productName")]
+#' 
+#' }
+neon_products <- function(
+  fields = c("productCode", 
+             "productName", 
+             "productStatus", 
+             "productDescription", 
+             "productStatus",
+             "themes",
+             "keywords",
+             "productCategory",
+             "productAbstract",
+             "productDesignDescription",
+             "productRemarks",
+             "productSensor",
+             "productPublicationFormatType",
+             "productHasExpanded",
+             "productBasicDescription", 
+             "productExpandedDescription"),
+  api = "https://data.neonscience.org/api/v0",
+  .token = Sys.getenv("NEON_TOKEN")){
+  
+  # consider a local cache option?
   
   resp <- httr::GET(paste0(api, "/products"),
                     httr::add_headers("X-API-Token" = .token))
   txt <- httr::content(resp, as="text")
   products <-jsonlite::fromJSON(txt)[[1]]
+  
+  # un-list character columns
+  products$themes <- 
+    vapply(products$themes, paste0, character(1L), collapse = " | ")
+  products$keywords <- 
+    vapply(products$keywords, paste0, character(1L), collapse = " | ")
+  
+  if(!is.null(fields))
+    products <- products[fields]
+  
+  # allow tibble-style printing for tidyverse users
+  class(products) <- c("tbl_df", "tbl", "data.frame")
   products
   
 }
@@ -26,6 +81,7 @@ neon_products <- function(api = "https://data.neonscience.org/api/v0",
 
 #' product <- "DP1.10003.001"
 
+#' 
 #' @importFrom httr GET content stop_for_status
 #' @importFrom jsonlite fromJSON
 neon_data <- function(product, 
@@ -59,8 +115,15 @@ neon_data <- function(product,
   
     
   ## Extract the file list from the data endpoint.  O(sites * months) calls
-  ## Consider optional progress indicator here!
-  resp <- lapply(data_api, httr::GET, httr::add_headers("X-API-Token" = .token))
+  pb <- progress::progress_bar$new(
+    format = "  querying API for available data [:bar] :percent eta: :eta",
+    total = length(data_api), 
+    clear = FALSE, width= 60)
+  
+  resp <- lapply(data_api, function(x){
+    if(!quiet) pb$tick()$print()
+    httr::GET(x, httr::add_headers("X-API-Token" = .token))
+  })
   
   
   
@@ -80,12 +143,13 @@ neon_data <- function(product,
 }
 
 
-neon_dir <- neon_default_registry <- function(){
+neon_dir <- function(){
   Sys.getenv("NEONSTORE_HOME", 
              rappdirs::user_data_dir("neonstore"))
 }
  
-#neon_contentstore <- function(files, dir = contentid::content_dir() ){
+#neon_contentstore <- function(files, dir = contentid::content_dir()){
+#  
 #  files$ids <- vapply(files$url, contentid::store, character(1L), dir = dir)
 #  files
 #}
@@ -95,6 +159,28 @@ neon_dir <- neon_default_registry <- function(){
 #' ".*basic.*[.]zip"
 
 
+#' Download NEON data products into a local store
+#' @param product A NEON productCode. See [neon_download].
+#' @param start_date Download only files as recent as (YYYY-MM-DD). Leave
+#' as `NA` to download up to the most recent avaialble data.
+#' @param end_date Download only files up to end_date (YYYY-MM-DD). Leave as 
+#' `NA` to download all prior data.
+#' @param file_regex Download only files matching this pattern.  See details.
+#' @param quiet Should progress be displayed?
+#' @param dir Location where files should be downloaded. By default will
+#' use the appropriate applications directory for your system 
+#' (see [rappdirs::user_data_dir]).  This default also be configured by
+#' setting the environmental variable `NEONSTORE_HOME`, see [Sys.setenv] or
+#' [Renviron].
+#' @param api the URL to the NEON API, leave as default.
+#' @param .token an authentication token from NEON. A token is not
+#' required but will allow access to a higher number of requests before
+#' rate limiting applies, see 
+#' <https://data.neonscience.org/data-api/rate-limiting/#api-tokens>.
+#' Note that once files are downloaded once, `neonstore` provides persitent
+#' access to them without further interaction required with the API.
+#' 
+#' @details `"*basic*.zip`
 #' 
 #' @export
 #' @examples 
@@ -102,15 +188,15 @@ neon_dir <- neon_default_registry <- function(){
 #'  
 #'  neon_download("DP1.10003.001", 
 #'                start_date = "2019-01-01", 
-#'                file_regex = "[.]csv")
+#'                file_regex = ".*basic.*[.]zip")
 #' 
 #' }
 neon_download <- function(product, 
                           start_date = NA,
                           end_date = NA,
-                          file_regex = "[.]csv",
+                          file_regex =  ".*basic.*[.]zip",
                           quiet = FALSE,
-                          dest = neon_dir(), 
+                          dir = neon_dir(), 
                           api = "https://data.neonscience.org/api/v0",
                           .token =  Sys.getenv("NEON_TOKEN")){
   
@@ -122,12 +208,12 @@ neon_download <- function(product,
                     .token = .token)
   
   ## Omit those files we already have
-  already_have <- list.files(dest)
+  already_have <- list.files(dir)
   new_data <- data[!(data$name %in% already_have), ]
   
   ## Filter for only files matching the file regex
   files <- new_data[grepl(file_regex, new_data$name),]
-  files$dest <- file.path(dest, files$name)
+  files$dir <- file.path(dir, files$name)
   
   ## Filter duplicate files, e.g. have identical crc32 values
   unique_files <- take_first_match(files, "crc32")
@@ -136,16 +222,30 @@ neon_download <- function(product,
   
   
   ## make sure destination exists
-  dir.create(dest, showWarnings = FALSE, recursive = TRUE)
+  dir.create(dir, showWarnings = FALSE, recursive = TRUE)
   
   ## now time to download!
+  
+  pb <- progress::progress_bar$new(
+    format = "  downloading [:bar] :percent eta: :eta",
+    total = length(data_api), 
+    clear = FALSE, width= 60)
+  
   handle <- curl::new_handle()
   for(i in seq_along(unique_files$url)){
-    curl::curl_download(unique_files[i, "url"], unique_files[i, "dest"], 
-                        quiet = quiet, handle = handle)
+    if(!quiet) pb$tick()$print()
+    curl::curl_download(unique_files[i, "url"], 
+                        unique_files[i, "dir"], 
+                        handle = handle)
   }
 
-  invisible(dest)  
+  
+  # unzip zip files
+  lapply(list.files(dir, "[.]zip", full.names = TRUE),
+         unzip, exdir = dir)
+  
+  
+  invisible(unique_files)
 }
 
 
--- R/store.R
+++ R/store.R
@@ -15,13 +15,12 @@ neon_store <- function(table = NULL, dir = neon_dir()){
   site <- "(NEON\\.D\\d\\d\\.\\w{4})\\."                     # \\1
   product <- "(DP\\d\\.\\d{5}\\.\\d{3})\\."                  # \\2
   name <- "(:?\\w+)?\\.?"                                    # \\3 
-  month <- "(:?\\d{4}-\\d{2})?\\.?"                          # \\5
-  type <- "(:?basic|expanded)?\\.?"                          # \\6
-  timestamp <- "(\\d{8}T\\d{6}Z)\\."                         # \\7
-  stamp <- paste0("(:?", month, type, ")?", timestamp)       # \\4
-  ext <- "(\\w+$)"                                           # \\8
-  regex <- paste0(site, product, name, stamp, ext)
-  meta <- strsplit(gsub(regex, "\\1  \\2  \\3  \\5  \\6  \\7  \\8", files), "  ")
+  month <- "(:?\\d{4}-\\d{2})?\\.?"                          # \\4
+  type <- "(:?basic|expanded)?\\.?"                          # \\5
+  timestamp <- "(:?\\d{8}T\\d{6}Z)?\\.?"                     # \\6
+  ext <- "(\\w+$)"                                           # \\7
+  regex <- paste0(site, product, name, month, type, timestamp, ext)
+  meta <- strsplit(gsub(regex, "\\1  \\2  \\3  \\4  \\5  \\6  \\7", files), "  ")
   
   ## Confirm parsing was successful
   parts <- vapply(meta, length, integer(1L))
@@ -83,7 +82,6 @@ neon_read <- function(table, dir = neon_dir()){
 
 # birds <- fs::dir_ls("birds") %>% vroom::vroom()
 
-
 #' x <- c(
 #' "NEON.D01.BART.DP1.10003.001.brd_countdata.2015-06.expanded.20191107T154457Z.csv",
 #' "NEON.D01.BART.DP0.10003.001.validation.20191107T152154Z.csv",
@@ -92,5 +90,4 @@ neon_read <- function(table, dir = neon_dir()){
 #' "NEON.D01.BART.DP1.10003.001.2019-06.basic.20191205T150213Z.zip"
 #' )
 #' strsplit(gsub(regex, "\\1  \\2  \\3  \\5  \\6  \\7  \\8", x), "  ")
-
 
