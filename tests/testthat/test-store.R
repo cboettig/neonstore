cd6ba0a39d49614c2e1152728a34f7de5f0b5bcc
Carl <cboettig@gmail.com>
2020-06-02 18:20:07 +0000

    a few more tests for store

--- tests/testthat/test-store.R
+++ tests/testthat/test-store.R
@@ -16,14 +16,15 @@ test_that("neon_index()", {
   
 })
 
-
 test_that("neon_store()", {
   
   x <- neon_store()
   expect_true(any(grepl("brd_countdata", x)))
   
+  x <- neon_store(dir = tempfile())
+  expect_null(x)
+  
 })
-
 
 test_that("neon_read()", {
   
@@ -31,5 +32,24 @@ test_that("neon_read()", {
   expect_is(x, "data.frame")
   expect_true(any(grepl("observerDistance", colnames(x))))
   
+})
+
+test_that("vroom_ragged()", {
+  
+  A <- data.frame(A = 1:5, B = 1:5)
+  B <- data.frame(A = 1:5, B = 1:5, C = 1:5)
+  
+  files <- c(file.path(neon_dir(), "A.txt"),
+             file.path(neon_dir(), "B.txt"))
+  vroom::vroom_write(A, files[[1]])
+  vroom::vroom_write(B, files[[2]])
+  
+  out <- vroom_ragged(files)
+  
+  expect_is(out, "data.frame")
+  expect_equal(colnames(out), colnames(B))
+  
   
 })
+
+
