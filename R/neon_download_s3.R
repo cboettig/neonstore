8f4f2cb1bce4aad36484afcb07b854ac91b36e22
Carl Boettiger <cboettig@gmail.com>
2020-08-11 05:31:56 +0000

    cran always tests the donttest examples, can cause failures if an API is down temporarily.

--- R/neon_download_s3.R
+++ R/neon_download_s3.R
@@ -20,10 +20,10 @@
 #' }
 #' 
 #' \donttest{
-#'  neon_download_s3("DP1.10003.001", 
-#'                   start_date = "2018-01-01", 
-#'                   end_date = "2019-01-01",
-#'                   site = "YELL")
+#'  neon_download("DP1.10003.001", 
+#'                start_date = "2018-01-01", 
+#'                end_date = "2019-01-01",
+#'                site = "YELL")
 #' }
 #' 
 #' \dontshow{
