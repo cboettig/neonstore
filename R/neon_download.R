be95ec9d3dc3fb53a4ef240f29d0b304a44f7c1c
Carl Boettiger <cboettig@gmail.com>
2020-09-22 19:53:20 +0000

    bugfix on checking for pre-downloaded files

--- R/neon_download.R
+++ R/neon_download.R
@@ -142,7 +142,7 @@ download_filters <- function(files, file_regex,
   if(is.null(files)) return(invisible(NULL)) # nothing to download
   
   ## Omit those files we already have
-  files <- files[!(files$name %in% list.files(dir)), ]
+  files <- files[!(files$name %in% basename(list.files(dir))), ]
   
   ## Filter for only files matching the file regex
   files <- files[grepl(file_regex, files$name), ]
