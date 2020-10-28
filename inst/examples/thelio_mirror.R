ab738d195d04988c338fa1bc42bce7273d07abce
Carl Boettiger <cboettig@gmail.com>
2020-10-28 16:10:15 +0000

    fix docs

--- inst/examples/thelio_mirror.R
+++ inst/examples/thelio_mirror.R
@@ -15,12 +15,11 @@ neonstore::neon_download(product="DP1.00094.001", site = ter_sites, type="basic"
 aq_sites <- c("BARC", "FLNT")
 neonstore::neon_download(product="DP1.20053.001",site = aq_sites)
 neonstore::neon_download(product="DP1.20288.001",site = aq_sites)
-
+neonstore::neon_download("DP1.20264.001", site =  aq_sites, type = "basic")
 
 ## import into local database
 neon_store(product = "DP1.00094.001", table = "SWS_30")
 
-## restart connection 
 neon_store(product = "DP1.10003.001")
 neon_store(product = "DP1.10022.001")
 neon_store(product = "DP1.10093.001")
