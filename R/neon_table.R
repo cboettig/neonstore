6f466e3f862a07f3ad086a59288b54e39f77f58d
Carl <cboettig@gmail.com>
2021-12-22 05:21:22 +0000

    update docs

--- R/neon_table.R
+++ R/neon_table.R
@@ -6,6 +6,8 @@
 #' We cannot filter on start_date or end_date since these
 #' come only from the filename metadata and are only added
 #' to instrument tables, not observation tables etc.
+#' @param type filter for basic or expanded. Can be ommitted unless you have 
+#' imported both types a given table into your database.
 #' @param lazy logical, default FALSE. Should we return a remote dplyr 
 #' connection to the table in duckdb? This can substantially improve 
 #' performance and avoid out-of-memory errors when working with very large
